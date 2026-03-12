e89597a9793ce2b96390e3dab5e478d5d8badcab
adamhasani <84440477+adamhasani@users.noreply.github.com>
2026-03-13 01:24:27 +0700

    Update screenshot.js

--- api/screenshot.js
+++ api/screenshot.js
@@ -7,17 +7,21 @@ export default async function handler(req, res) {
     if (!url) return res.status(400).send('Linknya mana?');
 
     try {
-        // Settingan khusus biar enteng di Vercel
         const browser = await puppeteer.launch({
             args: chromium.args,
-            defaultViewport: { width: 1920, height: 1080 }, // Mode laptop
+            defaultViewport: { width: 1920, height: 1080 }, // Mode laptop 1080p
             executablePath: await chromium.executablePath(),
             headless: chromium.headless,
             ignoreHTTPSErrors: true,
         });
         
         const page = await browser.newPage();
-        await page.goto(url, { waitUntil: 'networkidle2' });
+        
+        // Cuma nunggu struktur web muncul, maksimal 8 detik biar Vercel gak ngamuk
+        await page.goto(url, { 
+            waitUntil: 'domcontentloaded', 
+            timeout: 8000 
+        });
         
         const screenshot = await page.screenshot({ encoding: 'base64' });
         await browser.close();
@@ -25,6 +29,6 @@ export default async function handler(req, res) {
         res.status(200).json({ image: `data:image/png;base64,${screenshot}` });
     } catch (error) {
         console.error(error);
-        res.status(500).send('Server Vercel ngos-ngosan, gagal jepret.');
+        res.status(500).send('Server Vercel ngos-ngosan atau webnya keberatan, gagal jepret.');
     }
 }
